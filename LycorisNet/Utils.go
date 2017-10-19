c2027f260d1b474c5a007e8edf91192f49497785
RootHarold <1124403217@qq.com>
2017-10-19 11:45:23 +0800

    building

--- LycorisNet/Utils.go
+++ LycorisNet/Utils.go
@@ -35,6 +35,54 @@ func Tanh(f float64) float64 {
 	return (math.Exp(f) - math.Exp(-f)) / (math.Exp(f) + math.Exp(-f))
 }
 
+func Sort1(m *map[Gen]Ome, InnovationNum int) (*[]float64, *[]int) {
+	var length = len(*m)
+	var result1 = make([]float64, length)
+	var result2 = make([]int, length)
+	var temp1 = make([]bool, InnovationNum)
+	var temp2 = make([]float64, InnovationNum)
+	var temp3 = make([]int, InnovationNum)
+	var count = 0
+	for _, v := range *m {
+		in := v.InnovationNum
+		temp1[in] = true
+		temp2[in] = v.Weight
+		temp3[in] = in
+	}
+	for k, v := range temp1 {
+		if v {
+			result1[count] = temp2[k]
+			result2[count] = temp3[k]
+			count++
+		}
+	}
+	return &result1, &result2
+}
+
+func Sort2(m *map[Gen]Ome, InnovationNum int) (*[]Gen, *[]Ome) {
+	var length = len(*m)
+	var result1 = make([]Gen, length)
+	var result2 = make([]Ome, length)
+	var temp1 = make([]bool, InnovationNum)
+	var temp2 = make([]Gen, InnovationNum)
+	var temp3 = make([]Ome, InnovationNum)
+	var count = 0
+	for k, v := range *m {
+		in := v.InnovationNum
+		temp1[in] = true
+		temp2[in] = k
+		temp3[in] = v
+	}
+	for k, v := range temp1 {
+		if v {
+			result1[count] = temp2[k]
+			result2[count] = temp3[k]
+			count++
+		}
+	}
+	return &result1, &result2
+}
+
 func Error(output []float64, desire []float64) float64 {
 	var f float64 = 0
 	for i := 0; i < len(output); i++ {
@@ -82,30 +130,6 @@ func Distance(in1 *Individual, in2 *Individual) float64 {
 	return distance
 }
 
-func Sort1(m *map[Gen]Ome, InnovationNum int) (*[]float64, *[]int) {
-	var length = len(*m)
-	var result1 = make([]float64, length)
-	var result2 = make([]int, length)
-	var temp1 = make([]bool, InnovationNum)
-	var temp2 = make([]float64, InnovationNum)
-	var temp3 = make([]int, InnovationNum)
-	var count = 0
-	for _, v := range *m {
-		in := v.InnovationNum
-		temp1[in] = true
-		temp2[in] = v.Weight
-		temp3[in] = in
-	}
-	for k, v := range temp1 {
-		if v {
-			result1[count] = temp2[k]
-			result2[count] = temp3[k]
-			count++
-		}
-	}
-	return &result1, &result2
-}
-
 func mateIndividual(in1 *Individual, in2 *Individual) *Individual {
 	var offspring = Individual{InputNum: in1.InputNum, OutputNum: in1.OutputNum}
 	offspring.NodeMap = make(map[int]Node)
@@ -114,6 +138,37 @@ func mateIndividual(in1 *Individual, in2 *Individual) *Individual {
 		var temp = in1
 		in1 = in2
 		in2 = temp
+	}
+	var g1, o1 = Sort2(&in1.GenomeMap, in1.InnovationNum)
+	var g2, o2 = Sort2(&in2.GenomeMap, in2.InnovationNum)
+	var point1 = 0
+	var point2 = 0
+	var len1 = len(*g1)
+	var len2 = len(*g2)
+	for true {
+		if point1 == len1 || point2 == len2 {
+			break
+		}
+		if (*o1)[point1].InnovationNum == (*o2)[point2].InnovationNum {
+			if r.Float64() < 0.5 {
+				offspring.GenomeMap[(*g1)[point1]] = (*o1)[point1]
+			} else {
+				offspring.GenomeMap[(*g2)[point2]] = (*o2)[point2]
+			}
+			point1++
+			point2++
+		} else if (*o1)[point1].InnovationNum > (*o2)[point2].InnovationNum {
+			point2++
+		} else {
+			offspring.GenomeMap[(*g1)[point1]] = (*o1)[point1]
+			point1++
+		}
+	}
+	for i := point1; i < len1; i++ {
+		offspring.GenomeMap[(*g1)[point1]] = (*o1)[point1]
+	}
+	for i := point2; i < len2; i++ {
+		offspring.GenomeMap[(*g2)[point2]] = (*o2)[point2]
 	}
 	// TODO
 	return &offspring
