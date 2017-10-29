dcf467d14c8ad6e845ce06c93c510065d67a9c8e
RootHarold <1124403217@qq.com>
2017-10-29 20:39:16 +0800

    building

--- LycorisNet/Node.go
+++ LycorisNet/Node.go
@@ -1,18 +1,25 @@
 package LycorisNet
 
+// Every node contains its number, type and the value calculated
+// during "forward" function. The "genomeMap" provides faster
+// calculation because of the copy of genomes' map. When it comes
+// to the "nodeType", "0" means "input" while "1" means "hidden"
+// and "2" means "output". Each node is the basic unit of the gene.
 type node struct {
 	nodeNum   int
-	nodeType  int //0 means input while 1 means hidden and 2 means output
+	nodeType  int
 	value     float64
 	genomeMap map[gen]ome
 }
 
+// Create new node with its number and type.
 func newNode(nodeNum int, nodeType int) *node {
 	var n = &node{nodeNum: nodeNum, nodeType: nodeType}
 	n.genomeMap = make(map[gen]ome)
 	return n
 }
 
+// Deep clone of node.
 func (n *node) clone() *node {
 	var duplicate = newNode(n.nodeNum, n.nodeType)
 	duplicate.genomeMap = make(map[gen]ome, len(n.genomeMap))
