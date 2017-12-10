7d575d49ca9745637fcc5afc3242a789270334dc
RootHarold <1124403217@qq.com>
2017-12-10 23:52:59 +0800

    building

--- LycorisNet/Utils.go
+++ LycorisNet/Utils.go
@@ -168,6 +168,141 @@ func distance(in1 *individual, in2 *individual) float64 {
 // Mating two different individuals.
 func mateIndividual(in1 *individual, in2 *individual) *individual {
 	var offspring = individual{inputNum: in1.inputNum, outputNum: in1.outputNum}
+	offspring.nodeMap = make(map[int]node)
+
+	if in1.fitness > in2.fitness {
+		var temp = in1
+		in1 = in2
+		in2 = temp
+	}
+
+	var tempMap = make(map[int]int)
+	for _, v := range in1.nodeMap {
+		tempMap[v.nodeNum] = -1
+	}
+	var duplicateNodes []bool
+	for _, v := range in2.nodeMap {
+		_, ok := tempMap[v.nodeNum]
+		if ok {
+			duplicateNodes = append(duplicateNodes, true)
+		} else {
+			tempMap[v.nodeNum] = -1
+			duplicateNodes = append(duplicateNodes, false)
+		}
+	}
+	var tempSlice = make([]int, len(duplicateNodes))
+	var tempCount = 0
+	for k := range tempMap {
+		tempSlice[tempCount] = k
+		tempMap[k] = tempCount
+		tempCount++
+	}
+
+	var basicNodeSum = in1.inputNum + in1.outputNum
+	for k, v := range tempSlice {
+		var n node
+		if v < offspring.inputNum {
+			n = *newNode(v, 0)
+		} else if v >= offspring.inputNum && v < basicNodeSum {
+			n = *newNode(v, 2)
+			if duplicateNodes[k] {
+				if r.Float64() < 0.5 {
+					n.bias = in1.nodeMap[v].bias
+				} else {
+					n.bias = in2.nodeMap[v].bias
+				}
+			}
+		} else {
+			n = *newNode(v, 1)
+			if duplicateNodes[k] {
+				if r.Float64() < 0.5 {
+					n.bias = in1.nodeMap[v].bias
+				} else {
+					n.bias = in2.nodeMap[v].bias
+				}
+			}
+		}
+		offspring.nodeMap[v] = n
+	}
+
+	var g1, o1 = sort2(in1)
+	var g2, o2 = sort2(in2)
+	var point1 = 0
+	var point2 = 0
+	var len1 = len(*g1)
+	var len2 = len(*g2)
+	for true {
+		if point1 == len1 || point2 == len2 {
+			break
+		}
+		if (*o1)[point1].innovationNum == (*o2)[point2].innovationNum {
+			if r.Float64() < 0.5 {
+				offspring.nodeMap[(*g1)[point1].out].genomeMap[(*g1)[point1]] = (*o1)[point1]
+			} else {
+				offspring.nodeMap[(*g2)[point2].out].genomeMap[(*g2)[point2]] = (*o2)[point2]
+			}
+			point1++
+			point2++
+		} else if (*o1)[point1].innovationNum > (*o2)[point2].innovationNum {
+			offspring.nodeMap[(*g2)[point2].out].genomeMap[(*g2)[point2]] = (*o2)[point2]
+			point2++
+		} else {
+			offspring.nodeMap[(*g1)[point1].out].genomeMap[(*g1)[point1]] = (*o1)[point1]
+			point1++
+		}
+	}
+	for i := point1; i < len1; i++ {
+		offspring.nodeMap[(*g1)[point1].out].genomeMap[(*g1)[point1]] = (*o1)[point1]
+	}
+	for i := point2; i < len2; i++ {
+		offspring.nodeMap[(*g2)[point2].out].genomeMap[(*g2)[point2]] = (*o2)[point2]
+	}
+
+	var bt = 0
+	for true {
+		bt++
+		var flag = true
+		for _, v := range offspring.nodeMap {
+			for k := range v.genomeMap {
+				var indexIn = tempMap[k.in]
+				var indexOut = tempMap[k.out]
+				if indexIn > indexOut {
+					flag = false
+					tempSlice[indexIn] = k.out
+					tempSlice[indexOut] = k.in
+					tempMap[k.out] = indexIn
+					tempMap[k.in] = indexOut
+				}
+			}
+		}
+		if flag || bt == breakTime {
+			break
+		}
+	}
+
+	offspring.nodeSlice = make([]int, len(tempSlice))
+	for i := 0; i < in1.inputNum; i++ {
+		offspring.nodeSlice[i] = i
+	}
+	var point = in1.inputNum
+	for _, v := range tempSlice {
+		if v >= in1.inputNum {
+			offspring.nodeSlice[point] = v
+			point++
+		}
+	}
+
+	if in1.nodeSum > in2.nodeSum {
+		offspring.nodeSum = in1.nodeSum
+	} else {
+		offspring.nodeSum = in2.nodeSum
+	}
+
+	if in1.innovationNum > in2.innovationNum {
+		offspring.innovationNum = in1.innovationNum
+	} else {
+		offspring.innovationNum = in2.innovationNum
+	}
 
 	return &offspring
 }
