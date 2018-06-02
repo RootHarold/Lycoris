1e5671715c8cfb528d18f039bd88e78d7533d3f2
rootharold <rootharold@163.com>
2018-06-03 00:38:14 +0800

    building

--- LycorisNet/include/lycoris.h
+++ LycorisNet/include/lycoris.h
@@ -8,33 +8,54 @@
 
 class Lycoris {
 public:
-    Individual *best;
-    uint32_t capacity;
     uint32_t inputNum;
     uint32_t outputNum;
-    std::vector<Species *> *speciesList;
-    Args *args;
+    uint32_t capacity;
+    Individual *best;
 
+    Lycoris(uint32_t capacity, uint32_t inputNum, uint32_t outputNum);
+
+    ~Lycoris();
+
+    static std::string version();
+
+    void setForwardFunc(void (*forwardFunc)(Individual &in));
+
+    void setActivateFunc(std::string function);
+
+    void runLycoris();
+
+    void openMemLimit(uint32_t size);
+
+    void closeMemLimit();
+
+    void setMaxTock(uint32_t num);
+
+    void setGapLength(uint32_t num);
+
+    void setDistanceThreshold(float threshold);
+
+    void setDistanceArgs(float a, float b);
+
+    void setMaxMutateTime(uint32_t num);
+
+    void setWeigthRandom(float a, float b);
+
+    void setBiasRandom(float a, float b);
+
+    static Lycoris *importLycoris(std::string path, uint32_t capacity);
+
+    void exportLycoris(std::string path);
+
+private:
+    Args *args;
+    std::vector<Species *> *speciesList;
     std::vector<std::vector<Individual *>> tempList1;
     std::vector<std::vector<uint32_t >> tempList2;
     uint32_t specieLength;
     std::vector<uint32_t> oldLength;
 
     void (*forwardFunc)(Individual &in);
-
-    Lycoris(uint32_t capacity, uint32_t inputNum, uint32_t outputNum);
-
-    ~Lycoris();
-
-    std::string version();
-
-    void setMaxTock(uint32_t num);
-
-    void setForwardFunc(void (*forwardFunc)(Individual &in));
-
-    void setGapLength(uint32_t num);
-
-    void setDistanceThreshold(float threshold);
 
     void mate();
 
@@ -57,26 +78,6 @@ public:
     void autoParameter();
 
     void chooseElite();
-
-    void runLycoris();
-
-    void openMemLimit(uint32_t size);
-
-    void closeMemLimit();
-
-    void setDistanceArgs(float a, float b);
-
-    void setMaxMutateTime(uint32_t num);
-
-    void setWeigthRandom(float a, float b);
-
-    void setBiasRandom(float a, float b);
-
-    void setActivateFunc(std::string function);
-
-    static Lycoris *importLycoris(std::string path, uint32_t capacity);
-
-    void exportLycoris(std::string path);
 };
 
 #endif //LYCORIS_LYCORIS_H
