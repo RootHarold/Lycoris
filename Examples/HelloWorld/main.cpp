d5fd4abe574a41a1810b27f1b31dd87d79eb0e9b
rootharold <rootharold@163.com>
2018-09-23 22:51:48 +0800

    Add HelloWorld

---
+++ Examples/HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+/*
+    Copyright (c) 2018, RootHarold
+    All rights reserved.
+
+    Use of this source code is governed by an Apache license that can be found
+    in the LICENSE file.
+*/
+
+#include <iostream>
+#include "lycoris.h"
+
+using namespace LycorisNet;
+
+int main() {
+    std::cout << "Powered by: " << LycorisUtils::version() << std::endl;
+    return 0;
+}
